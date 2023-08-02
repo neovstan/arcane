f1f9e8e4daf243bf32dcdf93716e49f501ff34ae
neovstan <neovstan@gmail.com>
2023-08-02 20:05:00 +0500

    first commit

--- shared/psdk_utils/src/psdk_utils/weapon.cpp
+++
@@ -1,9 +0,0 @@
-#include "weapon.h"
-
-#include "psdk_utils.h"
-
-using namespace psdk_utils;
-
-CWeaponInfo& weapon::get_info(const eWeaponType weapon) {
-  return *CWeaponInfo::GetWeaponInfo(weapon, player()->m_nWeaponSkill);
-}
